3147ccbd1901591a6502b18b4c38a727d0bbb426
Adam Cox <mr.adamcox@gmail.com>
2025-04-05 17:32:39 -0500

    handle system settings on db setup

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -2,6 +2,13 @@
 
 psql -U postgres -c "DROP DATABASE lcai"
 psql -U postgres -c "CREATE DATABASE lcai"
+
+python manage.py es delete_indexes
+python manage.py es setup_indexes
 python manage.py migrate
 python manage.py createcachetable
-python manage.py packages -o load_package -s pkg --yes
+
+python manage.py packages -o import_graphs -s lcai/system_settings/Arches_System_Settings_Model.json
+python manage.py packages -o import_business_data -s lcai/system_settings/System_Settings.json -ow overwrite
+
+python manage.py packages -o load_package -s pkg
